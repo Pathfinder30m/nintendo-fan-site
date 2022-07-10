23ce7b4de4ef832726b69b0a8f024b4dbf7deea2
Pathfinder30m <mykhaylik@gmail.com>
2022-07-10 17:18:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nintendo-fan-site
+ 
---
+++ script.js
@@ -0,0 +1,70 @@
+function mainContent() {
+    document.getElementById("mainContent").setAttribute("class", "main");
+    document.getElementById("techInfo").setAttribute("class", "hidden");
+    document.getElementById("ways2play").setAttribute("class", "hidden");
+    document.getElementById("popGames").setAttribute("class", "hidden");
+    document.getElementById("accesories").setAttribute("class", "hidden");
+    document.getElementById("where2buy").setAttribute("class", "hidden");
+    document.getElementById("ingamefoto").setAttribute("class", "hidden");
+};
+function techInfo() {
+    document.getElementById("mainContent").setAttribute("class", "hidden");
+    document.getElementById("techInfo").setAttribute("class", "main");
+    document.getElementById("ways2play").setAttribute("class", "hidden");
+    document.getElementById("popGames").setAttribute("class", "hidden");
+    document.getElementById("accesories").setAttribute("class", "hidden");
+    document.getElementById("where2buy").setAttribute("class", "hidden");
+    document.getElementById("ingamefoto").setAttribute("class", "hidden");
+};
+function ways2play() {
+    document.getElementById("mainContent").setAttribute("class", "hidden");
+    document.getElementById("techInfo").setAttribute("class", "hidden");
+    document.getElementById("ways2play").setAttribute("class", "main");
+    document.getElementById("popGames").setAttribute("class", "hidden");
+    document.getElementById("accesories").setAttribute("class", "hidden");
+    document.getElementById("where2buy").setAttribute("class", "hidden");
+    document.getElementById("ingamefoto").setAttribute("class", "hidden");
+};
+function popgames() {
+    document.getElementById("mainContent").setAttribute("class", "hidden");
+    document.getElementById("techInfo").setAttribute("class", "hidden");
+    document.getElementById("ways2play").setAttribute("class", "hidden");
+    document.getElementById("popGames").setAttribute("class", "main");
+    document.getElementById("accesories").setAttribute("class", "hidden");
+    document.getElementById("where2buy").setAttribute("class", "hidden");
+    document.getElementById("ingamefoto").setAttribute("class", "hidden");
+};
+function accesories() {
+    document.getElementById("mainContent").setAttribute("class", "hidden");
+    document.getElementById("techInfo").setAttribute("class", "hidden");
+    document.getElementById("ways2play").setAttribute("class", "hidden");
+    document.getElementById("popGames").setAttribute("class", "hidden");
+    document.getElementById("accesories").setAttribute("class", "main");
+    document.getElementById("where2buy").setAttribute("class", "hidden");
+    document.getElementById("ingamefoto").setAttribute("class", "hidden");
+};
+function wheretobuy() {
+    document.getElementById("mainContent").setAttribute("class", "hidden");
+    document.getElementById("techInfo").setAttribute("class", "hidden");
+    document.getElementById("ways2play").setAttribute("class", "hidden");
+    document.getElementById("popGames").setAttribute("class", "hidden");
+    document.getElementById("accesories").setAttribute("class", "hidden");
+    document.getElementById("where2buy").setAttribute("class", "main");
+    document.getElementById("ingamefoto").setAttribute("class", "hidden");
+};
+function ingamefoto() {
+    document.getElementById("mainContent").setAttribute("class", "hidden");
+    document.getElementById("techInfo").setAttribute("class", "hidden");
+    document.getElementById("ways2play").setAttribute("class", "hidden");
+    document.getElementById("popGames").setAttribute("class", "hidden");
+    document.getElementById("accesories").setAttribute("class", "hidden");
+    document.getElementById("where2buy").setAttribute("class", "hidden");
+    document.getElementById("ingamefoto").setAttribute("class", "main");
+};
+/*document.getElementById("header").addEventListener("click", mainContent);
+document.getElementById("specs").addEventListener("click", techInfo);
+document.getElementById("ways").addEventListener("click", ways2play);
+document.getElementById("pop").addEventListener("click", popgames);
+document.getElementById("acc").addEventListener("click", accesories);
+document.getElementById("where").addEventListener("click", wheretobuy);
+document.getElementById("screen").addEventListener("click", ingamefoto);*/
